b956fd8dbe34043d191ec228938377990803448d
Edvanio Teixeira <76007866+edvanioteixeira@users.noreply.github.com>
2025-01-12 15:40:31 -0300

    Criei o ex002

---
+++ ex002.py
@@ -0,0 +1,9 @@
+nota1 = float(input('Digite a 1ª nota: '))
+nota2 = float(input('Digite a 2ª nota: '))
+media = (nota1 + nota2) / 2
+print('A média das notas é {:.1f}'.format(media))
+if (media >= 6):
+    print('Aprovado')
+else:
+    print('Reprovado')
+    
